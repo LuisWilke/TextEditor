1aea7c81d839a4b40caadfa30cfe2007e9dd82ba
LuisWilke <wilke_lopes@hotmail.com>
2024-01-24 09:19:19 -0400

    :tada:

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer? ");
+            Console.WriteLine("1 = Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            
+        }
+    }
+}
